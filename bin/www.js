4282b31658cd6100908f6c74cd4f53e6eebfd600
大鹏 <dapeng@192.168.1.103>
2022-02-09 06:10:13 -0800

    wip

--- bin/www.js
+++ bin/www.js
@@ -1,7 +1,9 @@
 #! /usr/bin/env node
+const path=require("path");
+const resolve=(package)=>path.join(__dirname,'..','node_modules',package)
 require("@babel/register")({
-  presets: ["@babel/preset-env", "@babel/preset-typescript"],
-  plugins: ["@babel/plugin-transform-runtime", "@babel/plugin-syntax-dynamic-import"],
+  presets: [resolve("@babel/preset-env"), resolve("@babel/preset-typescript")],
+  plugins: [resolve("@babel/plugin-transform-runtime"), resolve("@babel/plugin-syntax-dynamic-import")],
   extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
   ignore: [/node_modules/],
   cache: false,
